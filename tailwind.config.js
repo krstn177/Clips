72deaa818d86efe8fa2c4f5b84d8accf62e4f20c
krstn177 <christiangeshev@gmail.com>
2022-12-12 23:06:38 +0200

    Changing color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   content: ['./src/**/*.{html,ts}'],
   safelist: ['bg-blue-400', 'bg-green-400', 'bg-red-400'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'deep-blue': '#001233',
+        'coral-red': '#FF595A',
+        'beige': '#CAC0B3',
+        'less-deep-blue': '#002233',
+        'darker': '#000718'
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
